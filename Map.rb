48361c565247c9671d08875c78382b6c59bac1f1
bfuchs2 <bernardtfuchs@gmail.com>
2014-04-17 01:06:53 -0500

    changed the way movement works, added static variable @@SPEED to the GameObject class

--- Map.rb
+++ Map.rb
@@ -76,9 +76,17 @@ class Map
   end
 end
 
+module Direction
+  Up = 0
+  Right = 1
+  Down = 2
+  Left = 3
+  Still = 4 #for when the object isn't moving
+end
 class GameObject
-  attr_reader :x, :y
+  attr_reader :x, :y, :dir
   def initialize(window, x, y, image)
+    @@SPEED = 3
     @x, @y = x, y
     @map = window.map
     @tilesize = window.map.tilesize
@@ -91,19 +99,33 @@ class GameObject
   def warp(newx, newy)
     @x, @y = newx, newy
   end
-  def update(vx,vy)
-    if vx > 0
-      vx.times { if isValid?(vx+1,vy); @x+=vx; end}
+  def update(dir = @dir)
+    if(dir == Direction::Still)
+      dir = @dir
+    else
+      @dir = dir
     end
-    if vx < 0
-      (-vx).times { if isValid?(vx-1,vy); @x+=vx; end}
+    if(dir == Direction::Down)
+      @@SPEED.times { if isValid?(0, 1); @y+=1; end}
+    elsif(dir == Direction::Left)
+      @@SPEED.times { if isValid?(-1, 0); @x-=1; end}
+    elsif(dir == Direction::Up)
+      @@SPEED.times { if isValid?(0, -1); @y-=1; end}
+    elsif(dir == Direction::Right)
+      @@SPEED.times { if isValid?(1, 0); @x+=1; end}
     end
-    if vy > 0
-      vy.times { if isValid?(vx,vy-2); @y-=vy; end}
-    end
-    if vy < 0
-      (-vy).times { if isValid?(vx,vy+2); @y-=vy; end}
-    end
+    # if vx > 0
+      # vx.times { if isValid?(vx+1,vy); @x+=vx; end}
+    # end
+    # if vx < 0
+      # (-vx).times { if isValid?(vx-1,vy); @x+=vx; end}
+    # end
+    # if vy > 0
+      # vy.times { if isValid?(vx,vy-2); @y-=vy; end}
+    # end
+    # if vy < 0
+      # (-vy).times { if isValid?(vx,vy+2); @y-=vy; end}
+    # end
   end
 end
 
@@ -112,13 +134,11 @@ class Player < GameObject
     super
     @image, @image2, @image3, @image4 = *Image.load_tiles(window, image, 15, 15, false)
   end
-  def draw()
+  def draw
     @image.draw(@x, @y, 1, 1.0, 1.0)
   end
   def fail?(player_x, player_y, ghost_x, ghost_y)
-    if player_x == ghost_x && player_y == ghost_y
-      
-    end
+    player_x == ghost_x && player_y == ghost_y
   end
 end
 
@@ -153,13 +173,14 @@ class Game < Window
   end
   
   def update
-    vx = vy = 0
-    vx += 1 if button_down? KbRight
-    vx -= 1 if button_down? KbLeft
-    vy += 1 if button_down? KbUp
-    vy -= 1 if button_down? KbDown
-    @player.update(vx, vy)  
-    end
+    dir = Direction::Still
+    dir = Direction::Right if button_down? KbRight
+    dir = Direction::Left if button_down? KbLeft
+    dir = Direction::Up if button_down? KbUp
+    dir = Direction::Down if button_down? KbDown
+    @player.update(dir)  
+  end
+  
 end
 
 
