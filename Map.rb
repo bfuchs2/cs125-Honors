87eba4b7ca6882e7b7a8855bb476ac295659ad77
bfuchs2 <bernardtfuchs@gmail.com>
2014-04-18 16:52:20 -0500

    map generation now works but is statistically imbalanced

--- Map.rb
+++ Map.rb
@@ -22,42 +22,59 @@ class Map
     lines = File.readlines(filename).map { |line| line.chomp }
     @height = lines.size
     @width = lines[0].size
-    @tiles = generate
+    generate
   end
   
   def generate
-     tiles = Array.new(@width, Array.new(@height, Tiles::Space))
-     for x in 0..@width-1
-       for y in 0..@height-1
-         tiles[x][y] = Tiles::Wall
+     @tiles = Array.new(@width){ |i|
+        Array.new(@height, Tiles::Space)
+      }
+     for l in 0..@rand.rand(3..10) #generates horizontal lines randomly
+       line = [@rand.rand(@width), @rand.rand(@height), @rand.rand(1..10)] #format [midx, midy, length]
+       for mover in line[0]-line[2]/2..line[0]+line[2]/2
+         @tiles[mover][line[1]] = Tiles::Wall
        end
      end
+     for l in 0..@rand.rand(3..10) #same thing now with vertical lines
+       line = [@rand.rand(@width), @rand.rand(@height), @rand.rand(1..10)] #format [midx, midy, length]
+       for mover in line[1]-line[2]/2..line[1]+line[2]/2
+         @tiles[line[0]][mover] = Tiles::Wall
+       end
+     end
+     for x in 0..@width-1
+       for y in 0..@height-1
+         if(getSurrounding(x, y).length < 2)
+           @tiles[x][y] = Tiles::Wall
+         end
+       end
+     end
+     @tiles
   end
   
   def getSurrounding(x, y)#returns a list of the coords surrounding (x, y) that can be walked on
-    array = []
-    if(@tiles[x][y + 1] == Tiles::Wall)
+    array = Array.new
+    if(x < @tiles.length and @tiles[x][y + 1] == Tiles::Wall)
       array.push([x, y+1])
     end
-    if(@tiles[x + 1][y + 1] == Tiles::Wall)
+    if(x + 1 < @tiles.length and @tiles[x + 1][y + 1] == Tiles::Wall)
       array.push([x + 1, y + 1])
     end
-    if(@tiles[x + 1][y] == Tiles::Wall)
+    if(x + 1< @tiles.length and @tiles[x + 1][y] == Tiles::Wall)
       array.push([x + 1, y])
     end
-    if(@tiles[x + 1][y - 1] == Tiles::Wall)
+    if(x + 1< @tiles.length and @tiles[x + 1][y - 1] == Tiles::Wall)
       array.push([x + 1, y-1])
     end
-    if(@tiles[x][y - 1] == Tiles::Wall)
+    if(x < @tiles.length and @tiles[x][y - 1] == Tiles::Wall)
       array.push([x, y -1])
     end
-    if(@tiles[x - 1][y - 1] == Tiles::Wall)
+    if(x - 1< @tiles.length and @tiles[x - 1][y - 1] == Tiles::Wall)
       array.push([x - 1, y - 1])
     end
-    if(@tiles[x - 1][y] == Tiles::Wall)
+    if(x - 1< @tiles.length and @tiles[x - 1][y] == Tiles::Wall)
       array.push([x - 1, y])
     end
-    if(@tiles[x - 1][y + 1] == Tiles::Wall)
+    if(x - 1< @tiles.length and @tiles[x - 1][y + 1] == Tiles::Wall)
       array.push([x - 1, y + 1])
     end
     array
@@ -68,7 +85,7 @@ class Map
       @width.times do|x|
         tile = @tiles[x][y]
         if tile == Tiles::Wall
-          @tileset[tile].draw(x*@@tilesize, y*@@tilesize, 0)
+          @tileset[tile].draw(x*@tilesize, y*@tilesize, 0)
         end
       end
     end
