7e0d3eb7e130adb67c63875830c29b12a479a5a3
bfuchs2 <bernardtfuchs@gmail.com>
2014-04-29 14:46:31 -0500

    added comment, see previous commit messages

--- honors_project.rb
+++ honors_project.rb
@@ -5,6 +5,7 @@ require_relative 'ghost.rb'
 require 'gosu'
 include Gosu
 
+#format: Game.new(horizontal_pixels, vertival_pixels) note: both should be multiples of 15
 window = Game.new(5*40*15/2, 5*30*15/2)
 window.show
 #ghost_x, ghost_yr
